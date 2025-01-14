9b264a799c888ce2ccac76b9d60eb15253a19752
ivan perkov <ivan1996perkov@gmail.com>
2025-01-14 21:49:04 +0200

    new function

--- py.py
+++ py.py
@@ -1,1 +1,16 @@
 print("homework")
+
+def say():
+    print("Helo")
+
+say()
+
+def lst_even(lst):
+    for i in lst:
+        if i % 2 != 0:
+            print(i)
+
+s = lst_even([1, 2, 3, 4, 5, 6, 7])
+print(s)
+
+
